1f18ab35b46166dca644467fec83a35c3231aab5
xotic <xoticc.ja@gmail.com>
2023-04-29 14:09:33 -0400

    SECOND PAGE!

--- scrape.py
+++ scrape.py
@@ -7,14 +7,22 @@ soup = BeautifulSoup(res.text, 'html.parser')
 links = soup.select('.titleline > a')
 subtext = soup.select('.subtext')
 
+res2 = requests.get('https://news.ycombinator.com/news?p=2')
+soup2 = BeautifulSoup(res2.text, 'html.parser')
+links2 = soup2.select('.titleline > a')
+subtext2 = soup2.select('.subtext')
+
+mega_link = links + links2
+mega_subtext = subtext + subtext2
+
 def sort_stories_by_votes(hn):
     return sorted(hn, key= lambda k:k['votes'], reverse = True)
 
 def create_custom_hn(links,subtext):
     hn = []
     for idx, item in enumerate(links):
-        title = links[idx].getText()
-        href = links[idx].get('href')
+        title = item.getText()
+        href = item.get('href', None)
         vote = subtext[idx].select('.score')
         if len(vote):
             points = int(vote[0].getText().replace(' points', ''))
@@ -22,4 +30,4 @@ def create_custom_hn(links,subtext):
                 hn.append({'title': title, 'link': href, 'votes': points})
     return sort_stories_by_votes(hn)
 
-pprint.pprint(create_custom_hn(links,subtext)) 
+pprint.pprint(create_custom_hn(mega_link,mega_subtext)) 
